6f44d6cc34b1410e8d5cf354152c37a507ce23fe
salty-lychee <157775463+salty-lychee@users.noreply.github.com>
2024-01-26 12:37:05 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 basic.forever(function () {
     basic.showString("Hello!")
+    while (false) {
+        music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+    }
 })
